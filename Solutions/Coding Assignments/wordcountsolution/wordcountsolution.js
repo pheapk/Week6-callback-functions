f81057c78e0038d4ce2dc7e78dd1b57bcfb80495
Pheap <spheapk@gmail.com>
2021-11-02 00:13:04 -0700

    update, solution to word count

--- Solutions/Coding Assignments/wordcountsolution/wordcountsolution.js
+++ Solutions/Coding Assignments/wordcountsolution/wordcountsolution.js
@@ -8,11 +8,13 @@ window.onload = () => {
 function countWords() {
   let text =
     'Some of the biggest and most expensive transportation projects in the world have involved building bridges. Bridges are crucial links that carry cars, trucks and trains across bodies of water, mountain gorges or other roads. As a result, they are one of the most important aspects of civil engineering and are subject to intense scrutiny, especially when they collapse. Bridge collapses can be tragic events, leading to loss of life and serious property damage. That is why bridge engineers, designers and builders must always take their jobs very seriously. The best way for them to prevent these accidents is to understand why bridges collapse in the first place. Understanding bridge collapses can lead to major changes in the design, construction and safety of future building projects. The following are main reasons why bridges fall.';
-  // remove the periods and commas and make all lowercase letters
-  text = text.replace(/[.,]/g, '');
+  // remove the periods and commas
+  text = text.replace(/[.,]/g, ''); 
+  //make all lowercase letters
   text = text.toLocaleLowerCase();
   const wordArray = text.split(' ');
   console.log(wordArray);
+
   // create object and make keys the "words" and the value the "word count"
   const wordCount = {};
   wordArray.forEach((item) => {
@@ -25,7 +27,13 @@ function countWords() {
   let myArray = Object.entries(wordCount);
 
   // now sort the arrays based on the count number
+  // a[1] is the second value of the array
+  // a[1] < b[1] ? 1, so the sort is desc, biggest to smallest
+  // a[1] > b[1] ? 1, sort asc, smallest to biggest
+  // after the sort fuction return the sorted array, call the slice function to copy the first three items
+  // and return an array of those three items
   let bArray = myArray.sort((a, b) => (a[1] < b[1] ? 1 : a[1] > b[1] ? -1 : 0)).slice(0, 3);
+  
   // print out first three most common words.
   return bArray;
 }
